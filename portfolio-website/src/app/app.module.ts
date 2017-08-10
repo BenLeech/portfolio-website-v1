4bd1ba19c11398157a33485edd24829591fa813b
Unknown <b.leech@hotmail.ca>
2017-08-09 20:25:37 -0600

    sass setup, adding component templates

--- portfolio-website/src/app/app.module.ts
+++ portfolio-website/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
