11a5cba71c9acd0184a0644ad4961c980cb0b4a0
Unknown <b.leech@hotmail.ca>
2017-08-26 18:19:27 -0600

    generated portfolio display component

--- portfolio-website/src/app/app.module.ts
+++ portfolio-website/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SplashComponent } from './splash/splash.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { PortfolioDisplayComponent } from './portfolio-display/portfolio-display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    PortfolioDisplayComponent
   ],
   imports: [
     BrowserModule
