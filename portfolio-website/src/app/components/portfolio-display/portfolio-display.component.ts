6a95c886b003cf2df5d80527e401e65adb6059b2
Unknown <b.leech@hotmail.ca>
2017-09-03 16:49:08 -0600

    started fleshing out portfolio element and footer

--- portfolio-website/src/app/components/portfolio-display/portfolio-display.component.ts
+++ portfolio-website/src/app/components/portfolio-display/portfolio-display.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-portfolio-display',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio-display.component.scss']
 })
 export class PortfolioDisplayComponent implements OnInit {
+
+  @Input() title: string;
 
   constructor() { }
 
---
+++ portfolio-website/src/app/model/Portfolio-Element.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by BenLeech on 2017-08-26.
+ */
