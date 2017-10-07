6d054f1daf79931a77b9bb46030a9e067ad05427
Unknown <b.leech@hotmail.ca>
2017-10-07 16:06:22 -0600

    added skill bars

--- portfolio-website/src/app/components/skill-bar/skill-bar.component.ts
+++ portfolio-website/src/app/components/skill-bar/skill-bar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-skill-bar',
+  selector: 'skill-bar',
   templateUrl: './skill-bar.component.html',
   styleUrls: ['./skill-bar.component.scss']
 })
 export class SkillBarComponent implements OnInit {
 
+  @Input() skillName: string = "";
+  @Input() skillPercent: number = 0;
+
   constructor() { }
 
   ngOnInit() {
+    if(this.skillPercent > 100)
+      this.skillPercent = 100;
+    if(this.skillPercent < 0)
+      this.skillPercent = 0;
+  }
+
+  getSkillPercent(): string{
+    return this.skillPercent + "%";
   }
 
 }
