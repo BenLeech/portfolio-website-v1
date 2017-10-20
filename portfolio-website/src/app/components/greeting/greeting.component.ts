3fd903fddfde2af1f9dce33a52b4cfa8988fb16d
Unknown <b.leech@hotmail.ca>
2017-10-19 23:01:05 -0600

    general improvements

--- portfolio-website/src/app/components/greeting/greeting.component.ts
+++ portfolio-website/src/app/components/greeting/greeting.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import {trigger, state, style, animate, transition} from '@angular/animations';
+
 
 @Component({
   selector: 'greeting',
   templateUrl: './greeting.component.html',
-  styleUrls: ['./greeting.component.scss']
+  styleUrls: ['./greeting.component.scss'],
+  animations: [
+    trigger('elementState', [
+      state('inactive', style({
+        opacity: 0,
+        transform: 'translateY(50px)'
+
+      })),
+      state('visible',   style({
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+      transition('* => visible', animate('500ms ease-out'))
+    ])
+    ]
 })
 export class GreetingComponent implements OnInit {
+
+  state: string = 'inactive';
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  triggerFadeIn(){
+    this.state = 'visible';
   }
 
 }
