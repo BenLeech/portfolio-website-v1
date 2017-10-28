c40666485b789b5ae73431ed3daf4ea260491569
Unknown <b.leech@hotmail.ca>
2017-10-28 17:04:16 -0600

    changed navbar dropdown to fullscreen

--- portfolio-website/src/app/components/navbar/navbar.component.ts
+++ portfolio-website/src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
 import {Component, OnInit, HostListener} from '@angular/core';
 import {NavLink} from "../../model/Nav-Link";
 import {NavigationService} from "../../services/navigation.service";
+import {trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  animations: [
+    trigger('navBarState', [
+      state('inactive', style({
+        'height': '0'
+      })),
+      state('mobileMenu',   style({'height': '100%' })),
+      transition('inactive <=> mobileMenu', [animate('200ms ease')]),
+    ])
+  ]
 })
 
 export class NavbarComponent implements OnInit {
@@ -15,6 +25,8 @@ export class NavbarComponent implements OnInit {
 
   showDropdown: boolean = false;
   showNav: boolean = false;
+
+  state: string = 'inactive';
 
   navLinks:Array<NavLink> = [];
 
@@ -29,6 +41,7 @@ export class NavbarComponent implements OnInit {
 
   toggleDropdown(){
     this.showDropdown = !this.showDropdown;
+    this.state = (this.showDropdown) ? 'mobileMenu' : 'inactive';
   }
 
   @HostListener('window:scroll', [])
