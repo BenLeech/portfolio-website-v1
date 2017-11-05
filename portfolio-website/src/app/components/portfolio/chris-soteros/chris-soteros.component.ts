a840d08815bdfdb8ac9fd1437676809b982bea4a
Unknown <b.leech@hotmail.ca>
2017-11-04 19:39:52 -0600

    added forward/back/menu buttons to portfolio project pages

--- portfolio-website/src/app/components/portfolio/chris-soteros/chris-soteros.component.ts
+++ portfolio-website/src/app/components/portfolio/chris-soteros/chris-soteros.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {NavigationService} from "../../../services/navigation.service";
+import {PortfolioService} from "../../../services/portfolio.service";
+import {PortfolioElement} from "../../../model/Portfolio-Element";
 
 @Component({
   selector: 'chris-soteros',
@@ -8,10 +10,38 @@ import {NavigationService} from "../../../services/navigation.service";
 })
 export class ChrisSoterosComponent implements OnInit {
 
-  constructor(private navigationService: NavigationService) { }
+  portfolioElement: PortfolioElement;
+
+  displayLeftArrow: boolean;
+  displayRightArrow: boolean;
+
+  nextTitle: string;
+  prevTitle: string;
+
+  constructor(private navigationService: NavigationService,
+              private portfolioService: PortfolioService) { }
 
   ngOnInit() {
     this.navigationService.setScrollPercentage(0);
+
+    this.portfolioElement = this.portfolioService.helloWorldProject;
+    this.portfolioService.setCurrentPortfolioElement(this.portfolioService.helloWorldProject);
+    this.displayRightArrow = !this.portfolioService.isLast(this.portfolioElement);
+    this.displayLeftArrow = !this.portfolioService.isFirst(this.portfolioElement);
+
+    if(this.displayRightArrow)
+      this.nextTitle = this.portfolioService.getNextTitle();
+
+    if(this.displayLeftArrow)
+      this.prevTitle = this.portfolioService.getLastTitle();
+  }
+
+  onNextArrowClick(){
+    this.portfolioService.goToNext();
+  }
+
+  onBackArrowClick(): any{
+    this.portfolioService.goBack();
   }
 
 }
--- portfolio-website/src/app/services/portfolio.service.ts
+++ portfolio-website/src/app/services/portfolio.service.ts
@@ -1,25 +1,96 @@
 import { Injectable } from '@angular/core';
 import {PortfolioElement} from "../model/Portfolio-Element";
+import {Router} from "@angular/router";
 
 const basePath = "/assets/img/portfolio/";
 
 @Injectable()
 export class PortfolioService {
 
-  private helloWorldProject: PortfolioElement =
+  helloWorldProject: PortfolioElement =
     {title: "University Professor Profile website", imagePath:basePath+"helloWorldProjectCover.png",
       tags: ['Web Development'], linkPath: '/uni-prof-portfolio'};
-  private miri: PortfolioElement =
+  miri: PortfolioElement =
     {title: "Miri", imagePath:basePath+"miri_mockup.jpg", tags: ['Game Development'], linkPath: '/miri-game'};
 
-  constructor() { }
+  portfolioWork: Array<PortfolioElement> = [];
+  featuredWork: Array<PortfolioElement> = [];
+
+  private currentPortfolioElement: PortfolioElement;
+
+  constructor(private router: Router) {
+    this.portfolioWork = [this.helloWorldProject, this.miri];
+    this.featuredWork = [this.helloWorldProject, this.miri];
+  }
 
   getFeaturedPortfolioWork(): Array<PortfolioElement>{
-    return [this.helloWorldProject, this.miri];
+    return this.featuredWork;
   }
 
   getPortfolioWork(): Array<PortfolioElement>{
-    return [this.helloWorldProject, this.miri];
+    return this.portfolioWork;
+  }
+
+  getCurrentPortfolioElement(): PortfolioElement{
+    return this.currentPortfolioElement;
+  }
+
+  setCurrentPortfolioElement(project: PortfolioElement){
+    this.currentPortfolioElement = project;
+  }
+
+  getIndex(project: PortfolioElement){
+    return this.portfolioWork.indexOf(project);
+  }
+
+  isFirst(project: PortfolioElement): boolean {
+    return project == this.portfolioWork[0];
+  }
+
+  isLast(project: PortfolioElement): boolean {
+    return project == this.portfolioWork[this.portfolioWork.length-1];
+  }
+
+  goToNext(){
+    const index = this.getIndex(this.currentPortfolioElement);
+    if(index == this.portfolioWork.length-1 || index == -1)
+      return;
+
+    this.currentPortfolioElement = this.portfolioWork[index + 1];
+    this.router.navigate([this.currentPortfolioElement.linkPath]);
+  }
+
+  goBack(){
+    const index = this.getIndex(this.currentPortfolioElement);
+    if(index <= 0)
+      return;
+
+    this.currentPortfolioElement = this.portfolioWork[index -1];
+    this.router.navigate([this.currentPortfolioElement.linkPath]);
+  }
+
+  goToFirst(){
+    this.currentPortfolioElement = this.portfolioWork[0];
+  }
+
+  goToLast(){
+    this.currentPortfolioElement = this.portfolioWork[this.portfolioWork.length-1];
+  }
+
+  getNextTitle(): string{
+    const index = this.getIndex(this.currentPortfolioElement);
+    if(index == this.portfolioWork.length-1 || index == -1)
+      return '';
+
+    return this.portfolioWork[index+1].title;
+  }
+
+  getLastTitle(): string{
+    const index = this.getIndex(this.currentPortfolioElement);
+    if(index <= 0)
+      return '';
+
+    return this.portfolioWork[index-1].title;
   }
 
 }
