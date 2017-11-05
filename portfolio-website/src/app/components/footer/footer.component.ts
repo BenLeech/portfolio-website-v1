4df3aa61ef54b204572ae5c4e04acb67bc3b38f8
Unknown <b.leech@hotmail.ca>
2017-11-05 13:20:16 -0600

    added scroll to top button

--- portfolio-website/src/app/components/footer/footer.component.ts
+++ portfolio-website/src/app/components/footer/footer.component.ts
@@ -1,19 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Observable, Subject} from "rxjs";
 
 @Component({
   selector: 'footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent implements OnInit, OnDestroy {
 
   linkedinPath: string = 'https://www.linkedin.com/in/ben-leech-4195b6126';
   githubPath: string = 'https://github.com/BenLeech';
   instagramPath: string = 'https://www.instagram.com/benleech_/';
 
+  scrollSubject: Subject<void> = new Subject<void>();
+
   constructor() { }
 
   ngOnInit() {
+    this.scrollSubject.switchMap(() => { return Observable.interval(1)
+      .scan((acc, curr) => acc - 35, window.pageYOffset)
+      .do(pos => window.scrollTo(0,pos))
+      .takeWhile(val => val > 0)}).subscribe();
+  }
+
+  ngOnDestroy(){
+    this.scrollSubject.unsubscribe();
+  }
+
+  scrollToTop(){
+    this.scrollSubject.next();
   }
 
 }
