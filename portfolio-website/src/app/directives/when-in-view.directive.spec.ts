b79d21b77e1a0ac3a4d38091da726d2f9196e2a6
Unknown <b.leech@hotmail.ca>
2017-11-19 21:34:01 -0600

    removed component tests

--- portfolio-website/src/app/directives/when-in-view.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { WhenInViewDirective } from './when-in-view.directive';
-
-describe('WhenInViewDirective', () => {
-  it('should create an instance', () => {
-    const directive = new WhenInViewDirective();
-    expect(directive).toBeTruthy();
-  });
-});
